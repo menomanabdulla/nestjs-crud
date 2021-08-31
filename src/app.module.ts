68b715da93cf1ab0c76400a46a3218a69b3bd860
noman abdulla <noman.fcibappy@gmail.com>
2021-08-31 17:44:50 +0600

    Connect mongodb with application

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,13 @@ import { AppService } from './app.service';
 import { ItemsController } from './items/items.controller';
 import { ItemsService } from './items/items.service';
 import { ItemsModule } from './items/items.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import config from './config/keys'
 
 @Module({
-  imports: [ItemsModule],
+  imports: [
+    MongooseModule.forRoot(config.mongoURI),
+    ItemsModule],
   controllers: [AppController, ItemsController],
   providers: [AppService, ItemsService],
 })
---
+++ src/config/keys.ts
@@ -0,0 +1,3 @@
+export default {
+    mongoURI: 'mongodb+srv://testDbUser:123test123@cluster0.qa80y.mongodb.net/Test?retryWrites=true&w=majority'
+}
